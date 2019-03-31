cedcd80766c009236694fd581d78028a4260b1ad
jordan birkeland <jbirkeland@mcad.edu>
2019-03-31 12:39:13 -0500

    Added Vue data to my html data, added 10 facts, and did simple css updates

---
+++ script.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#llamaFacts",
+    data: {
+        family: "Llamas are part of the camelid family",
+        group: "A group of llamas is called a herd",
+        height: "They can grow to be 6 feet tall",
+        weight: "They weigh between 280 and 450 pounds",
+        training: "They are smart and easy to train",
+        diet: "Llamas are vegetarian",
+        history: "They were used as pack animals 4,000 - 5,000 years ago in the Peruvian highlands",
+        lifespan: "Their life expectancy is about 20 years old",
+        offspring: "A baby llama is called a Cria",
+        lifestyle: "They are social animals"
+    }
+});
